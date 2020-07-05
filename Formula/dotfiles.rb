f42b0816073b874e8926909a4248f1ea5287b8b6
Mike McClenaghan <mike@sideline.ca>
2020-07-05 07:15:28 -0600

    Initial commit

---
+++ Formula/dotfiles.rb
@@ -0,0 +1,14 @@
+class Dotfiles < Formula
+  desc "Mike's dotfiles"
+  homepage "https://github.com/mm53bar/dotfiles"
+  head "https://github.com/mm53bar/dotfiles.git"
+
+  def install
+    #mv Dir.glob("foo-*").first, "foo"
+    #bin.install "foo"
+  end
+
+  test do
+    #foo help
+  end
+end
